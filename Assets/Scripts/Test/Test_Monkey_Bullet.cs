bf25855096a68898f27fc0841cd7918b14237954
DsystemSurface <simulator.research@gmail.com>
2017-06-02 16:41:28 +0900

    大塚製チュートリアルで生じたファイルを追加。

---
+++ Assets/Scripts/Test/Test_Monkey_Bullet.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Monkey_Bullet : MonoBehaviour {
+
+    new Rigidbody rigidbody;
+
+    // Use this for initialization
+    void Start() {
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.AddForce(transform.forward * 100, ForceMode.Impulse);
+
+		Destroy(gameObject, 3);
+    }
+	
+	void Update () {
+		
+	}
+
+	// OnCllisionEnterは、Unityが自動で呼ぶ関数
+	void OnCollisionEnter(Collision collision) {
+		if (collision.collider.name == "Monkey")
+		{
+			Debug.Log("ゲームクリア！");
+			Destroy(collision.collider.gameObject);
+		}
+	}
+	void OnTriggerEnter(Collider collider)
+	{
+		if (collider.name == "Monkey")
+		{
+			Debug.Log("ゲームクリア！");
+			Destroy(collider.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Test/Test_Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Player : MonoBehaviour {
+
+    CharacterController controller;
+    public GameObject bullet;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+
+		Debug.Log("Startが呼ばれた");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("Updateが呼ばれた");
+
+		var move = new Vector3(
+            Input.GetAxis("move_x"),
+            0,
+            Input.GetAxis("move_y")
+        );
+
+        controller.SimpleMove(transform.rotation * move * 10);
+
+        transform.Rotate(
+            transform.up,
+            Input.GetAxis("camera_x") * 180 * Time.deltaTime
+        );
+
+        if (Input.GetMouseButtonDown(0)) {
+            Instantiate(
+				bullet,
+				transform.position + transform.forward * 1,
+				transform.rotation);
+        }
+	}
+}
